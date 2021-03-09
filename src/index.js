d09a665dcfbc60328b3e1ac77e0b281971810924
unfunuk <a.s.s.t.e.r.i.x.0@gmail.com>
2021-03-09 19:07:34 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -12,4 +12,4 @@ module.exports = function toReadable(num) {
     str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'only ' : '';
     return str;
 }
-}
+
